38a00ef95e1f878ff40fec9b9f67cbfc6feb4499
cynen <cynen@foxmail.com>
2018-09-28 21:58:16 +0800

    快排

--- SortedAlgorithm/InsertionSort.py
+++ SortedAlgorithm/InsertionSort.py
@@ -19,14 +19,14 @@ def insertSort(list):
     return list
 
 
-############暂未实现.
+############
 def insertSort2(list):
     #从第2个元素开始向前插入. 拿数进行和前面的数据比较.
-    for i in range(1,len(list)):
+    for i in range(len(list)):
         # 从第i个元素开始向前比较，如果小于前一个元素，交换位置
-        for j in range(i,-1,-1):
-            if list[i] < list[j]:
-                list[j],list[i] = list[i],list[j]
+        for j in range(i,0,-1):
+            if list[j] < list[j-1]:
+                list[j],list[j-1] = list[j-1],list[j]
     return list
 
 
---
+++ SortedAlgorithm/QuickSort.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+
+
+
+"""
+    快速排序.
+    又称划分交换排序（partition-exchange sort），
+    通过一趟排序将要排序的数据分割成独立的两部分，
+    其中一部分的所有数据都比另外一部分的所有数据都要小，
+    然后再按此方法对这两部分数据分别进行快速排序，
+    整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+"""
+
+
+#
+# list 需要排序的列表, start 排序起始索引. end 排序终止索引.
+def quickSort(list,start,end):
+    '''快排'''
+    # if len(list) <= 1:
+    #     return
+    if start >= end:
+        return
+
+
+    mid_value = list[start]
+    low = start
+    high = end
+    while low < high:
+        # 由于取得中值是左边,应该先移动右边.(此顺序必须保证.)
+        while low < high and list[high] >= mid_value :
+            high -=1
+        list[low] = list[high] # 因为此时,low索引上的数据还是midvalue,可以被更改!!!
+        # 右边移动到比中值小的时候,就开始移动左边.
+        while low < high and list[low] < mid_value:
+            low +=1
+        list[high] = list[low]
+    #退出循环的时候,需要设置midvalue
+    list[low] = mid_value
+
+    # 快排左边.
+    quickSort(list,start,low-1);
+    # 快排右边.
+    quickSort(list,low+1,end);
+
+
+list = [54,26,93,17,77,31,44,55,20]
+print(list)
+quickSort(list,0,len(list)-1)
+print(list)
